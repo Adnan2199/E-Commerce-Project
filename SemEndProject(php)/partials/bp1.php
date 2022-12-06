4c163b964001c4d527f7dc9e778eb62a0b6c38f0
Adnan Khan <118618204+Adnan2199@users.noreply.github.com>
2022-12-07 01:24:06 +0530

    Add files via upload

---
+++ SemEndProject(php)/partials/bp1.php
@@ -0,0 +1,7 @@
+<section id="bigpicture">
+    <h4>Special-Offer</h4>
+    <h2>Super Value deals</h2>
+    <h1>For Students</h1>
+    <p>Save more with Us </p>
+    <button>Shop Now</button>
+</section>
