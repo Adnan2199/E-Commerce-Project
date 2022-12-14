3faabe3d31d59eb8e2b25a43386a87e82184c1fe
Adnan Khan <118618204+Adnan2199@users.noreply.github.com>
2022-12-14 07:55:02 -0800

    Add files via upload

---
+++ SemEndProject(php)/partials/aboutstuff.php
@@ -0,0 +1,57 @@
+<section id="details">
+    <div class="info">
+        <h2>Get in Touch with Us</h2>
+        <p>
+            <i class="far fa-map"></i></p>
+            <p>
+            Basavanagudi
+        </p>
+        <p>
+            <i class="far fa-envelope"></i></p>
+            <p>
+            ebuy@hotmail.com
+        </p>
+        <p>
+            <i class="far fa-phone-alt"></i></p>
+            <p>
+            123456789
+        </p>
+    </div>
+    <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.481412110949!2d77.56294551139413!3d12.941017415493844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae158b11e34d2f%3A0x5f4adbdbab8bd80f!2sBMS%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1669905630503!5m2!1sen!2sin" width="750" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+</section>
+<section id="rate">
+    <form>
+        <p>Write to Us</p>
+        <input type="text" placeholder="Enter your Name">
+        <input type="text" placeholder="Enter your Email">
+        <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+        <button>Submit</button>
+    </form>
+    <div class="members">
+        <div class="person">
+            <img src="Images/logo.png" alt="">
+            <p>Adnan <br>
+                2nd Year
+            </p>
+        </div>
+        <div class="person">
+            <img src="Images/logo.png" alt="">
+            <p>Likhith<br>
+                2nd Year
+            </p>
+        </div>
+        <div class="person">
+            <img src="Images/logo.png" alt="">
+            <p>Manav<br>
+                2nd Year
+            </p>
+        </div>
+        <div class="person">
+            <img src="Images/logo.png" alt="">
+            <p>Anant<br>
+                2nd Year
+            </p>
+        </div>
+
+    </div>
+</section>
