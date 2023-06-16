3cc43444d271fb44321755dec936238fa7a76a3b
Likhith096 <117436338+Likhith096@users.noreply.github.com>
2023-06-16 20:09:12 +0530

    Add files via upload

--- search.js
+++ search.js
@@ -1,8 +1,9 @@
+
 const search= ()=>{
-    const searchbox = document.getElementById("search-item").ariaValueMax.toUpperCase();
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
     const storeitems = document.getElementById("product-list")
     const product = document.querySelectorAll(".product")
-    const pname =document.getElementsByTagName("h2")
+    const pname =storeitems.getElementsByTagName("h2")
 
     for(var i=0;i<pname.length;i++){
         let match = product[i].getElementsByTagName("h2")[0];
